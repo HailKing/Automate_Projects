d5897b9dd0db4109bb870ad26b0f808833ab45b8
Hércules de Sousa <herculesdeso@gmail.com>
2020-05-29 15:08:53 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,17 @@
 
 creating(){
 	# Put here the path to the Automating Projects folder
-	current_directory=$PWD
 	automating_folder=""
+
+	current_directory=$PWD
 
 	cd
 	cd $automating_folder
+	python3 connection.py $1 $token $status 
+	if [ $? == 0 ]
+	then 
+		return 0
+	fi
 	source .env
 
 	cd $path_to_projects_folder
@@ -16,25 +22,22 @@ creating(){
 	else
 		if [ -z "$2" ] || [ $2 == "public" ]
 		then
-			echo "Defining status as public"
+			# echo "Defining status as public"
 			status=false
 		else
 			if [ $2 == "private" ]
 			then
-				echo "Defining status as private"
+				# echo "Defining status as private"
 				status=true
 			else
 				echo "Repository status must be public or private"
 				return 0
 			fi
 		fi
-		echo "Creating repository with name $1"
+		
+		echo "Creating repository on local machine with name $1"
 		mkdir $1
 		cd $1
-
-		echo "-------------------------------------------------------------------------------"
-		response=$(curl -H "Authorization: token $token" --data '{"name": "'"${1}"'", "auto_init":false, "private": '$status'}'  https://api.github.com/user/repos)
-		echo "-------------------------------------------------------------------------------"
 
 		git init
 		git remote add origin https://github.com/$user/$1.git
@@ -49,5 +52,6 @@ creating(){
 	fi
 	
 	cd $current_directory
+	return 1
 }
 
